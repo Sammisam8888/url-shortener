813af0e090426183e9d096add99307f68913541e
sammisam8888 <samuelpriyatam@gmail.com>
2025-03-28 10:09:29 +0530

    added new interactive frontend

--- static/script.js
+++ static/script.js
@@ -1,23 +1,11 @@
-document.getElementById("shorten-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    let originalUrl = document.getElementById("original_url").value;
-
-    fetch("/", {
-        method: "POST",
-        body: new URLSearchParams({ "original_url": originalUrl }),
-        headers: { "Content-Type": "application/x-www-form-urlencoded" }
-    })
-    .then(response => response.json())
-    .then(data => {
-        let shortUrl = data.short_url;
-        document.getElementById("short-url").value = shortUrl;
-        document.getElementById("result").style.display = "block";
-    });
-});
 
 function copyToClipboard() {
-    let shortUrlInput = document.getElementById("short-url");
-    shortUrlInput.select();
-    document.execCommand("copy");
-    alert("Short URL copied to clipboard!");
+    let shortUrlInput = document.getElementById("short-link");
+    if (shortUrlInput.value) {
+        shortUrlInput.select();
+        document.execCommand("copy");
+        alert("Short URL copied to clipboard!");
+    } else {
+        alert("No URL to copy!");
+    }
 }
